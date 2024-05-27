8321e95766220e1103a72ae293bc5bc4bfabb5d3
Gabriel <gcardozo@balanz.com>
2024-05-27 12:36:51 -0300

    saco log

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const {BuildRouting} = require('./src/classes');
 
 function init(pathModels, pathController) {
-    console.log(pathModels, pathController);
     let buildRouting = new BuildRouting(pathModels, pathController);
     buildRouting._findModels();
     return buildRouting._build();
